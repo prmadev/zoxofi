4d27ac225d9366bda7d5537a3b16f7e082233aba
amirography <amirhossein.alesheikh@gmail.com>
2022-10-18 14:09:27 +0330

    feat(): first working version

---
+++ src/main.rs
@@ -0,0 +1,44 @@
+use anyhow::Result;
+use rofi;
+use std::{io::Read, os::unix::process::CommandExt, process::Command};
+
+fn main() -> Result<()> {
+    let dirs: Vec<String> = get_zoxide_list()?
+        .iter()
+        .map(|x| String::from(format!("{}", x)))
+        .collect();
+
+    let a = rofi::Rofi::new(&&dirs)
+        .lines(10)
+        .width(rofi::Width::Percentage(40))?
+        .prompt("project to open in neovide:")
+        .run()?;
+    launch_neovide(&a)?;
+
+    Ok(())
+    // let choice = get_input(dirs)?;
+}
+
+// #[allow(unused)]
+// fn get_input(dirs: Vec<String>) -> Result<String> {}
+
+fn get_zoxide_list() -> Result<Vec<String>> {
+    let output = Command::new("zoxide").args(["query", "-l"]).output()?;
+
+    let mut buf: String = String::from("");
+
+    output.stdout.as_slice().read_to_string(&mut buf)?;
+
+    let result = format!("{}", &buf);
+
+    let lined: Vec<String> = result
+        .split('\n')
+        .map(|x| String::from(x.trim().replace("/home/a/", "")))
+        .collect();
+
+    Ok(lined)
+}
+
+fn launch_neovide(path: &str) -> Result<()> {
+    Err(Command::new("neovide").arg(path).exec().into())
+}
